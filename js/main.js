f68b7e620c8d2e4cb85fc70ad4eb85a57ee9b233
erinmilling <mill0814@algonquinlive.com>
2016-03-02 20:13:26 -0500

    added more vars

--- js/main.js
+++ js/main.js
@@ -1,8 +1,11 @@
-  var newLetter = prompt('Enter a letter:') function(newLetter) {
+  var newLetter = prompt('Enter a letter:');
+
+  var i = 0;
 
   for (i = 0; i x 2; i++) {
     document.write(newLetter + '<br>');
   }
 };
 
+var addLetter = function(newLetter) {
 newLetter('b');
